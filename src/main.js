8e84c9123aae22dac77313a7b9d05c54b660720f
Roo-CS <195388339+Roo-CS@users.noreply.github.com>
2025-03-03 19:59:09 -0500

    Subject: fixed boxes filling container
    Body:
    - the boxes were filling the container but they were hard-coded to always be 16x16
    - user input now changes how big the etch-a-sketch is up to 100 boxes
    - the box's height and width now change based on the user input to fill the container space

--- src/main.js
+++ src/main.js
@@ -1,13 +1,36 @@
 function createGrid(){
-  var container = document.querySelector(".container");
 
-  for(let i = 0; i < 16; i++){
-    for(let j = 0; j < 16; j++){
-      let box = document.createElement("div");
-      box.classList.add("box");
-      container.appendChild(box);
+  let container = document.querySelector(".container");
+  removeGrid(container);
+
+  let input = document.querySelector("input");
+  let gridSize = input.value;
+  let boxSize = getComputedBoxSize(container, gridSize);
+
+  if(gridSize > 1 && grid <= 100){
+    for(let i = 0; i < gridSize; i++){
+      for(let j = 0; j < gridSize; j++){
+        let box = document.createElement("div");
+        box.classList.add("box");
+        box.style.height = boxSize;
+        box.style.width = boxSize;
+        box.addEventListener("mouseover", ()=>{
+          box.style.backgroundColor = "blue";
+        });
+        container.appendChild(box);
+      }
     }
   }
 }
 
-createGrid();
+function removeGrid(container){
+  let child = container.lastElementChild;
+  while(child){
+    container.removeChild(child);
+    child = container.lastElementChild;
+  }
+}
+
+function getComputedBoxSize(container, gridSize){
+  return String((parseInt(window.getComputedStyle(container).getPropertyValue("height")) / gridSize) + "px");
+}
